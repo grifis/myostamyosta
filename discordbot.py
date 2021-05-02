55f74008e30bbaafcb3f50f573d6166ef86c9e9d
grifis <fujeba2abm@i.softbank.jp>
2021-05-02 11:39:13 +0900

    a

--- discordbot.py
+++ discordbot.py
@@ -48,6 +48,27 @@ async def on_command_error(ctx, error):
 async def on_ready():
 	print("on_ready")
 
+def omikuji_result():
+    num = random.randint(0,600)
+
+    if num < 20:
+        return "大吉"
+    elif 20 <= num < 100:
+        return "中吉"
+    elif 100 <= num < 200:
+        return "小吉"
+    elif 200 <= num < 400:
+        return "吉"
+    elif 400 <= num < 500:
+        return "末吉"
+    elif 500 <= num < 550:
+        return "凶"
+    elif 550 <= num < 580:
+        return "中凶"
+    elif 580 <= num < 581:
+        return "超大吉だよ！おめでとう！確率はなんと0.17%だよ！"
+    else:
+        return "大凶"
 
 def hand_to_int(hand):
     """
@@ -77,6 +98,9 @@ def get_player_result(player_hand, bot_hand):
     ]
     return result_table[player_hand][bot_hand]
 
+@bot.command()
+async def omikuji(ctx):
+    await ctx.send(omikuji_result())
 
 @bot.command()
 async def jyanken(ctx, hand):
