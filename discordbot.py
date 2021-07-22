989f58c4b077315a2c6d5b47a2902c8cd20573ae
grifis <fujeba2abm@i.softbank.jp>
2021-07-22 15:50:45 +0900

    a

--- discordbot.py
+++ discordbot.py
@@ -18,6 +18,7 @@ token = os.environ['DISCORD_BOT_TOKEN']
 embed = discord.Embed()
 embed.color = discord.Color.blue()
 lang = os.getenv('DISCORD_BOT_LANG', default='ja')
+channel_id = None
 
 batugame = [
     "自身の良いところを熱く語る", "語尾に「にゃん」と言う", "一位の人をご主人様と呼ぶ", "語尾にぴょんと言う",
@@ -307,6 +308,8 @@ async def group(ctx, specified_num=1):
 
 @bot.command()
 async def 接続(ctx):
+    global channel_id
+    channel_id = ctx.channel.id
     if ctx.message.guild:
         if ctx.author.voice is None:
             await ctx.send('ボイスチャンネルに接続してから呼び出してください。')
@@ -333,46 +336,47 @@ async def 切断(ctx):
 
 @bot.event
 async def on_message(message):
-    if message.content.startswith("/"):
-        pass
-    else:
-        if message.guild.voice_client:
-            text = message.content
-            text = text.replace('\n', '、')
-            pattern = r' ?<@(\d+)> '
-            match = re.findall(pattern, text)
-            for user_id in match:
-                user = await bot.fetch_user(user_id)
-                username = f'、{user.name}へのメンション、'
-                text = re.sub(f' ?<@{user_id}> ', username, text)
-            pattern = r'<:([a-zA-Z0-9_]+):\d+>'
-            match = re.findall(pattern, text)
-            for emoji_name in match:
-                emoji_read_name = emoji_name.replace('_', ' ')
-                text = re.sub(rf'<:{emoji_name}:\d+>', f'、{emoji_read_name}、', text)
-            pattern = r'https://tenor.com/view/[\w/:%#\$&\?\(\)~\.=\+\-]+'
-            text = re.sub(pattern, '画像', text)
-            pattern = r'https?://[\w/:%#\$&\?\(\)~\.=\+\-]+(\.jpg|\.jpeg|\.gif|\.png|\.bmp)'
-            text = re.sub(pattern, '、画像', text)
-            pattern = r'https?://[\w/:%#\$&\?\(\)~\.=\+\-]+'
-            text = re.sub(pattern, '、URL', text)
-            text =  text
-            if text[-1:] == 'w' or text[-1:] == 'W' or text[-1:] == 'ｗ' or text[-1:] == 'W':
-                while text[-2:-1] == 'w' or text[-2:-1] == 'W' or text[-2:-1] == 'ｗ' or text[-2:-1] == 'W':
-                    text = text[:-1]
-                text = text[:-1] + '、ワラ'
-            if message.attachments:
-                text += '、添付ファイル'
-            if len(text) < 100:
-                s_quote = urllib.parse.quote(text)
-                mp3url = f'http://translate.google.com/translate_tts?ie=UTF-8&q={s_quote}&tl={lang}&client=tw-ob'
-                while message.guild.voice_client.is_playing():
-                    await asyncio.sleep(0.5)
-                message.guild.voice_client.play(discord.FFmpegPCMAudio(mp3url))
-            else:
-                await message.channel.send('100文字以上は読み上げできません。')
-        else:
+    if message.channel.id == channel_id:
+        if message.content.startswith("/"):
             pass
+        else:
+            if message.guild.voice_client:
+                text = message.content
+                text = text.replace('\n', '、')
+                pattern = r' ?<@(\d+)> '
+                match = re.findall(pattern, text)
+                for user_id in match:
+                    user = await bot.fetch_user(user_id)
+                    username = f'、{user.name}へのメンション、'
+                    text = re.sub(f' ?<@{user_id}> ', username, text)
+                pattern = r'<:([a-zA-Z0-9_]+):\d+>'
+                match = re.findall(pattern, text)
+                for emoji_name in match:
+                    emoji_read_name = emoji_name.replace('_', ' ')
+                    text = re.sub(rf'<:{emoji_name}:\d+>', f'、{emoji_read_name}、', text)
+                pattern = r'https://tenor.com/view/[\w/:%#\$&\?\(\)~\.=\+\-]+'
+                text = re.sub(pattern, '画像', text)
+                pattern = r'https?://[\w/:%#\$&\?\(\)~\.=\+\-]+(\.jpg|\.jpeg|\.gif|\.png|\.bmp)'
+                text = re.sub(pattern, '、画像', text)
+                pattern = r'https?://[\w/:%#\$&\?\(\)~\.=\+\-]+'
+                text = re.sub(pattern, '、URL', text)
+                text =  text
+                if text[-1:] == 'w' or text[-1:] == 'W' or text[-1:] == 'ｗ' or text[-1:] == 'W':
+                    while text[-2:-1] == 'w' or text[-2:-1] == 'W' or text[-2:-1] == 'ｗ' or text[-2:-1] == 'W':
+                        text = text[:-1]
+                    text = text[:-1] + '、ワラ'
+                if message.attachments:
+                    text += '、添付ファイル'
+                if len(text) < 100:
+                    s_quote = urllib.parse.quote(text)
+                    mp3url = f'http://translate.google.com/translate_tts?ie=UTF-8&q={s_quote}&tl={lang}&client=tw-ob'
+                    while message.guild.voice_client.is_playing():
+                        await asyncio.sleep(0.5)
+                    message.guild.voice_client.play(discord.FFmpegPCMAudio(mp3url))
+                else:
+                    await message.channel.send('100文字以上は読み上げできません。')
+            else:
+                pass
     await bot.process_commands(message)
 
 
