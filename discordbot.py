8601698d0cd45a40eda88fff7954c4e65daee3ff
grifis <fujeba2abm@i.softbank.jp>
2021-08-03 14:09:36 +0900

    a

--- discordbot.py
+++ discordbot.py
@@ -352,32 +352,38 @@ async def on_message(message):
             pass
         else:
             if message.guild.voice_client:
-                text = message.content
-                text = text.replace('\n', '、')
-                pattern = r' ?<@(\d+)> '
-                match = re.findall(pattern, text)
-                for user_id in match:
-                    user = await bot.fetch_user(user_id)
-                    username = f'、{user.name}へのメンション、'
-                    text = re.sub(f' ?<@{user_id}> ', username, text)
-                pattern = r'<:([a-zA-Z0-9_]+):\d+>'
-                match = re.findall(pattern, text)
-                for emoji_name in match:
-                    emoji_read_name = emoji_name.replace('_', ' ')
-                    text = re.sub(rf'<:{emoji_name}:\d+>', f'、{emoji_read_name}、', text)
-                pattern = r'https://tenor.com/view/[\w/:%#\$&\?\(\)~\.=\+\-]+'
-                text = re.sub(pattern, '画像', text)
-                pattern = r'https?://[\w/:%#\$&\?\(\)~\.=\+\-]+(\.jpg|\.jpeg|\.gif|\.png|\.bmp)'
-                text = re.sub(pattern, '、画像', text)
-                pattern = r'https?://[\w/:%#\$&\?\(\)~\.=\+\-]+'
-                text = re.sub(pattern, '、URL', text)
-                text =  text
-                if text[-1:] == 'w' or text[-1:] == 'W' or text[-1:] == 'ｗ' or text[-1:] == 'W':
-                    while text[-2:-1] == 'w' or text[-2:-1] == 'W' or text[-2:-1] == 'ｗ' or text[-2:-1] == 'W':
-                        text = text[:-1]
-                    text = text[:-1] + '、ワラ'
-                if message.attachments:
-                    text += '、添付ファイル'
+            text = message.content
+            text = text.replace('\n', '、')
+            pattern = r'<@(\d+)>'
+            match = re.findall(pattern, text)
+            for user_id in match:
+                user = await client.fetch_user(user_id)
+                user_name = f'、{user.name}へのメンション、'
+                text = re.sub(f'<@{user_id}>', user_name, text)
+            pattern = r'<@&(\d+)>'
+            match = re.findall(pattern, text)
+            for role_id in match:
+                role = message.guild.get_role(int(role_id))
+                role_name = f'、{role.name}へのメンション、'
+                text = re.sub(f'<@&{role_id}>', role_name, text)
+            pattern = r'<:([a-zA-Z0-9_]+):\d+>'
+            match = re.findall(pattern, text)
+            for emoji_name in match:
+                emoji_read_name = emoji_name.replace('_', ' ')
+                text = re.sub(rf'<:{emoji_name}:\d+>', f'、{emoji_read_name}、', text)
+            pattern = r'https://tenor.com/view/[\w/:%#\$&\?\(\)~\.=\+\-]+'
+            text = re.sub(pattern, '画像', text)
+            pattern = r'https?://[\w/:%#\$&\?\(\)~\.=\+\-]+(\.jpg|\.jpeg|\.gif|\.png|\.bmp)'
+            text = re.sub(pattern, '、画像', text)
+            pattern = r'https?://[\w/:%#\$&\?\(\)~\.=\+\-]+'
+            text = re.sub(pattern, '、URL', text)
+            text = message.author.name + '、' + text
+            if text[-1:] == 'w' or text[-1:] == 'W' or text[-1:] == 'ｗ' or text[-1:] == 'W':
+                while text[-2:-1] == 'w' or text[-2:-1] == 'W' or text[-2:-1] == 'ｗ' or text[-2:-1] == 'W':
+                    text = text[:-1]
+                text = text[:-1] + '、ワラ'
+            if message.attachments:
+                text += '、添付ファイル'
                 if len(text) < 100:
                     s_quote = urllib.parse.quote(text)
                     mp3url = f'http://translate.google.com/translate_tts?ie=UTF-8&q={s_quote}&tl={lang}&client=tw-ob'
