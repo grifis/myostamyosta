22b41181d8cbf52b20f3d4b19d080eb3db216f12
grifis <fujeba2abm@i.softbank.jp>
2021-08-31 12:03:22 +0900

    modify mention

--- discordbot.py
+++ discordbot.py
@@ -375,13 +375,13 @@ async def on_message(message):
             if message.guild.voice_client:
                 text = message.content
                 text = text.replace('\n', '、')
-                pattern = r' ?<@(\d+)> '
+                pattern = r'<@(\d+)>'
                 match = re.findall(pattern, text)
                 for user_id in match:
                     user = await bot.fetch_user(user_id)
                     username = f'、{user.name}へのメンション、'
-                    text = re.sub(f' ?<@{user_id}> ', username, text)
-                pattern = r'<:([a-zA-Z0-9_]+):\d+>'
+                    text = re.sub(f'<@{user_id}>', username, text)
+                pattern = r'<@&(\d+)>'
                 match = re.findall(pattern, text)
                 for emoji_name in match:
                     emoji_read_name = emoji_name.replace('_', ' ')
