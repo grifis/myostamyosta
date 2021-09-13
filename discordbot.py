2bba9912586d2d021d1e92a422e39d2252db9a31
grifis <fujeba2abm@i.softbank.jp>
2021-09-13 21:57:59 +0900

    a

--- discordbot.py
+++ discordbot.py
@@ -10,6 +10,7 @@ from MakeTeam import MakeTeam
 from omikuji_result import omikuji_result
 import urllib.parse
 import re
+import yutube_dl
 
 intents = discord.Intents.default()
 intents.members = True
