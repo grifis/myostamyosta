6e0e5fda75c096627642b341cc36d0f41b0b28e4
grifis <fujeba2abm@i.softbank.jp>
2021-09-15 13:32:32 +0900

    a

--- discordbot.py
+++ discordbot.py
@@ -140,7 +140,7 @@ class Music(commands.Cog):
             ctx.voice_client.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
 
         await ctx.send('Now playing: {}'.format(player.title))
-    @commands.command(aliases=["fs"])
+    @commands.command(aliases=["fs", "s"])
     async def skip(self, ctx):
         await ctx.send(":fast_forward: Skipped :thumbsup:")
         await ctx.voice_client.pause()
@@ -496,7 +496,7 @@ async def group(ctx, specified_num=1):
 
 
 @bot.command()
-async def s(ctx):
+async def start(ctx):
     global channel_id
     guild_id = ctx.guild.id
     channel_id[guild_id] = ctx.channel.id
